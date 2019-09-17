859536a42e6e67a60c017985a44ca2efc4251e67
Arnav Gupta <arnav@codingblocks.com>
2019-09-17 10:36:57 +0530

    webpack chunk name
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -24,10 +24,7 @@ export default new Router({
     {
       path: '/faq',
       name: 'faq',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/FAQ.vue')
+      component: () => import(/* webpackChunkName: "faq" */ './views/FAQ.vue')
     }
   ]
 })
