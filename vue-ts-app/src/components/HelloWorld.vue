c8ff73fc8b033bd68c9d6990d4a40977ea49b21a
Arnav Gupta <arnav@codingblocks.com>
2019-09-25 12:21:46 +0530

    readonly prop
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- vue-ts-app/src/components/HelloWorld.vue
+++ vue-ts-app/src/components/HelloWorld.vue
@@ -39,7 +39,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop() private readonly msg!: string;
   @Prop() private name!: string;
   private myMsg: string = this.msg;
 
